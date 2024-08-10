e17c810af4433ffed7daa031cbadcb5689f7dc44
prad1101 <prajapatipradeep1101@gmail.com>
2024-08-10 16:22:17 +0530

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Delegates
+{
+    public delegate void printdelegate();
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            printdelegate p = new printdelegate(print);
+            Practice.Test(p);
+        }
+
+        private static void print()
+        {
+            Console.WriteLine("this is the print method");
+        }
+    }
+
+    class Practice
+    {
+        public static void Test(printdelegate p)
+        {
+            p();
+        }
+
+    }
+}
